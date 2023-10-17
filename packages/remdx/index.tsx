59c4e2c241cae4aebe13d791d64c9ec2384b6f17
cpojer <christoph.pojer@gmail.com>
2023-10-17 22:35:39 +0900

    Export `slidesToComponent`.

--- packages/remdx/index.tsx
+++ packages/remdx/index.tsx
@@ -7,4 +7,5 @@ export type {
 export { default as Deck } from './src/deck';
 export { default as render } from './src/render';
 export { default as Slide } from './src/slide';
+export { default as slidesToComponent } from './src/slidesToComponent';
 export { useMDXComponents } from '@mdx-js/react';
